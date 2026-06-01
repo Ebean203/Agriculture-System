a37c2636b602248218029a6339dc8e78ad392f40
ebean203 <tacandongrey@gmail.com>
2026-06-01 11:31:32 +0800

    fix: Remove unnecessary port number from MySQL connection in conn.php

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@ $host     = "localhost";
 $db_user  = "root";        // Your MySQL username
 $db_pass  = "";            // Your MySQL password (blank by default in XAMPP)
 $db_name  = "agriculture-system";
-$conn     = new mysqli($host, $db_user, $db_pass, $db_name,3307);
+$conn     = new mysqli($host, $db_user, $db_pass, $db_name,);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
